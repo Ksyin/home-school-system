1728492c3d4a5f860cdef3025d58b65a9084dea3
kysin <tituswamae2018@gmail.com>
2026-03-08 15:49:59 +0300

    Initial homeschool system commit

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,9 +1,9 @@
+
 export const firebaseConfig = {
   apiKey: "AIzaSyBRnnT1ZuzCEU_ZXrGFAJab7WtTgBOP3l0",
   authDomain: "home-school-1f592.firebaseapp.com",
   projectId: "home-school-1f592",
   storageBucket: "home-school-1f592.appspot.com",
   messagingSenderId: "1049462004266",
-  appId: "1:1049462004266:web:595973a4f9922d75eb4153",
-  measurementId: "G-1DTTVWM7R9"
+  appId: "1:1049462004266:web:595973a4f9922d75eb4153"
 };
