0e8e323ea4e170673bc6480303556a0c95369397
kysin <tituswamae2018@gmail.com>
2026-03-09 12:02:43 +0300

    Initial homeschool system commit

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,9 +1,9 @@
-// Replace with your real Firebase web app config from Firebase Console > Project settings > General > Your apps
 export const firebaseConfig = {
-  apiKey: "PASTE_API_KEY",
-  authDomain: "PASTE_PROJECT.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT.firebasestorage.app",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyBRnnT1ZuzCEU_ZXrGFAJab7WtTgBOP3l0",
+  authDomain: "home-school-1f592.firebaseapp.com",
+  projectId: "home-school-1f592",
+  storageBucket: "home-school-1f592.appspot.com",
+  messagingSenderId: "1049462004266",
+  appId: "1:1049462004266:web:595973a4f9922d75eb4153",
+  measurementId: "G-1DTTVWM7R9"
 };
